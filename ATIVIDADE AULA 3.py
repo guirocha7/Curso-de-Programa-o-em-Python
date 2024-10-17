79b8615bb435f91626a174e15268543ce90cd2fd
Guilherme Rocha <guilherme.rocha.7@outlook.com>
2024-10-17 17:58:00 -0300

    Add files via upload

---
+++ ATIVIDADE AULA 3.py
@@ -0,0 +1,21 @@
+#FUNÇÕES PRIMITIVAS 
+
+#Foi atribuido a numero o 10. Ou seja abriu espaço na memória RAM, chamado numero, e atribuiu o valor de 10. F5 dá o comando (da un run). código é lido de cima para baixo, da mesquerda para direita. 
+
+numero = 10
+numero = 30.5
+numero = 'ISSO É UM NÚMERO'
+
+print(numero)
+
+#Dado do tipo string (texto)
+texto = 'isso é um texto'
+print(texto)
+
+#Dado do tipo float (real)
+real = 10.5
+print(real)
+
+#Dado do tipo bool(lógico)
+boleano = True
+print(boleano)
